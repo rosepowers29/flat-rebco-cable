4fcc55d418b01720505403e065cf9ed33e9ff132
Rose Powers <rosepowers@dynamic-oit-ip4-wifirestricted04-10-51-97-237.princeton.edu>
2026-01-26 10:18:22 -0500

    updates to channel logic

--- clean_IC.py
+++ clean_IC.py
@@ -8,13 +8,33 @@ from pathlib import Path
 R_CABLE = 5.8e-5
 
 def get_tape_current(infile):
+    # get keys
+    keys = infile.columns
+    # find current and voltage channels
+    if 'CH3' in keys:
+        chV_tot = 'CH3'
+        if 'CH5' in keys:
+            chV_tape = 'CH5'
+        else:
+            chV_tape = 'CH6'
+    elif 'CH4' in keys:
+        chV_tot = 'CH4'
+        chV_tape = 'CH5'
+    elif 'CH5' in keys:
+        chV_tot = 'CH5'
+        chV_tape = 'CH6'        
+    else:
+        return None
     # fill these with the input data
-    V_tot = np.array(infile['CH3'])
-    V_tape = np.array(infile['CH5']) # should I be averaging channel 5 and channel 6??
+    V_tot = np.array(infile[chV_tot])
+    V_tape = np.array(infile[chV_tape])
     I_input = np.array(infile['CURRENT'])
 
-    I_cable = V_tot / R_CABLE
-    I_tape = I_input - I_cable
+    try:
+        I_cable = V_tot / R_CABLE
+        I_tape = I_input - I_cable
+    except Exception:
+        return None
 
     return I_tape, V_tape
 
@@ -25,9 +45,12 @@ args = parser.parse_args()
 
 
 for infile in args.infiles:
-    # sep lets us read in a tab-separated textfile as a csv
+    # sep \t lets us read in a tab-separated textfile as a csv
     csv_in = pd.read_csv(infile, sep = "\t")
-    I_tape, V_tape = get_tape_current(csv_in)
+    try:
+        I_tape, V_tape = get_tape_current(csv_in)
+    except Exception:
+        continue
     csv_in['TAPE_CURRENT'] = I_tape
     csv_in['TAPE_VOLTAGE'] = V_tape
     csv_in.to_csv(Path(infile).stem+'-cleaned.txt', sep='\t')
